7ef611af6f7492afcff271d2b353d4d5728f3822
Nakul Drupad Shah <ndshah@gogoair.com>
2017-10-25 16:12:53 -0500

    now with sample json

--- weather.py
+++ weather.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
+from interface.WeatherInfo import WeatherInfo
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/weather')
 def hello_weather():
-    return 'Hello This Is Your Weather API!'
+    output = WeatherInfo()
+    result = json.dumps(output.__dict__)
+    return jsonify(result)
 
 
 if __name__ == '__main__':
