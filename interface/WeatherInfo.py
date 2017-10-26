224ad7862cd0bb7a374e47e5b4311a6caa9a04c5
Nakul Drupad Shah <ndshah@gogoair.com>
2017-10-25 22:47:32 -0500

    some cleanup

--- interface/WeatherInfo.py
+++ interface/WeatherInfo.py
@@ -1,9 +1,9 @@
 class WeatherInfo:
-    city = 'Chicago' #default city is null
+    city = "Chicago" #default city is null
     temperature = '3 degrees Celcius' #default city temperature
     zipcode = 60614 #default city temperature
 
     def __init__(self):
-        self.city = 'Chicago' #default city is null
+        self.city = "Chicago" #default city is null
         self.temperature = '3 degrees Celcius' #default city temperature
         self.zipcode = 60614 #default city temperature
--- weather.py
+++ weather.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify
 from interface.WeatherInfo import WeatherInfo
-import json
 
 app = Flask(__name__)
 
@@ -8,8 +7,7 @@ app = Flask(__name__)
 @app.route('/weather')
 def hello_weather():
     output = WeatherInfo()
-    result = json.dumps(output.__dict__)
-    return jsonify(result)
+    return jsonify(output.__dict__)
 
 
 if __name__ == '__main__':
